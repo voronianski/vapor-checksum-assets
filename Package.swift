a723a40cb6b171f13728a074e87a22a0c11382e3
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-12-17 13:18:31 +0100

    add missed deps version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "VaporChecksumAssets",
   dependencies: [
-    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
+    .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
   ]
 )
